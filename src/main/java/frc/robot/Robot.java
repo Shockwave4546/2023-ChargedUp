324707b64ee33a0351e6f2074328255e7f7c6cca
Bqckword <linpan0328@gmail.com>
2023-01-28 18:47:54 -0500

    Initial commit

---
+++ src/main/java/frc/robot/Robot.java
@@ -0,0 +1,32 @@
+package frc.robot;
+
+import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj2.command.CommandScheduler;
+
+public class Robot extends TimedRobot {
+  private final RobotContainer container = new RobotContainer();
+
+  @Override public void robotInit() {
+ 
+  }
+
+  @Override public void robotPeriodic() {
+    CommandScheduler.getInstance().run();
+  }
+
+  @Override public void autonomousInit() {
+
+  }
+
+  @Override public void autonomousPeriodic() {
+
+  }
+
+  @Override public void teleopInit() {
+
+  }
+
+  @Override public void teleopPeriodic() {
+
+  }
+}
