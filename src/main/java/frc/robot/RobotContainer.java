a0ddca21e3f255cf0df1a30cdf545ba1326ce1cf
Bqckword <linpan0328@gmail.com>
2023-02-11 01:25:56 -0500

    Add debug tab

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -1,11 +1,17 @@
 package frc.robot;
 
+import frc.robot.commands.ResetArmPositionCommand;
 import frc.robot.subsystems.ArmSubsystem;
+import frc.robot.utils.shuffleboard.Tab;
 
 public class RobotContainer {
   private final ArmSubsystem arm = new ArmSubsystem();
 
   public RobotContainer() {
+    configureDebugTab();
+  }
 
+  private void configureDebugTab() {
+    Tab.DEBUG.add("Reset Arm Position", new ResetArmPositionCommand(arm));
   }
 }
--- src/main/java/frc/robot/utils/shuffleboard/Tab.java
+++ src/main/java/frc/robot/utils/shuffleboard/Tab.java
@@ -5,5 +5,6 @@ import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardTab;
 
 public class Tab {
   public static final ShuffleboardTab MATCH = Shuffleboard.getTab("Match");
+  public static final ShuffleboardTab DEBUG = Shuffleboard.getTab("Debug");
   public static final ShuffleboardTab MISC = Shuffleboard.getTab("Misc");
 } 
