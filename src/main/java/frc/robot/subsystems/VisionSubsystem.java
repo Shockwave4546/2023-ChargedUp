f36972cd6f55fc07d2cbfebf5be2c3ec0da0ce36
Bqckword <linpan0328@gmail.com>
2023-02-21 18:06:54 -0500

    vision!

---
+++ src/main/java/frc/robot/subsystems/VisionSubsystem.java
@@ -0,0 +1,11 @@
+package frc.robot.subsystems;
+
+import org.photonvision.PhotonCamera;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class VisionSubsystem extends SubsystemBase {
+  // TODO: Fill in the camera name, which would be in NT
+  private final PhotonCamera camera = new PhotonCamera("cameraName");
+
+}
